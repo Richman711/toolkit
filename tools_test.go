f6ad5f1ca54085bee161597b5b0b414c3e21d0e2
richard orman <richorman@gmail.com>
2023-03-29 14:54:08 +0900

    removed a depricated package ioutil

--- tools_test.go
+++ tools_test.go
@@ -5,7 +5,6 @@ import (
 	"image"
 	"image/png"
 	"io"
-	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
@@ -211,7 +210,7 @@ func TestTools_DownloadStaticFIle(t *testing.T) {
 		t.Error("wrong content disposition")
 	}
 
-	_, err := ioutil.ReadAll(res.Body)
+	_, err := io.ReadAll(res.Body)
 	if err != nil {
 		t.Error(err)
 	}
